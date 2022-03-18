02d20277c037c011e2376fee5712b3b01ee5eaca
Christopher Breen <C.jbreen61@gmail.com>
2022-03-18 13:07:56 -0400

    added map

--- src/pages/locations.js
+++ src/pages/locations.js
@@ -4,8 +4,7 @@ import Layout from "../components/layout"
 const Locations = () => (
 
 <Layout>
-<h1>Locations</h1>
-</Layout>
+<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d47269.55040698157!2d-73.97054972308867!3d40.73353670064665!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1zUm9iZXJ0YeKAmXM!5e0!3m2!1sen!2sus!4v1647376394225!5m2!1sen!2sus" width={'100%'} height={'400px'} style={{border: 1}} /></Layout>
 
 )
 
